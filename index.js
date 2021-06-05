cd998ebd0b2b9112caa9cb6c719824d49db672c6
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2021-06-05 22:26:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js');require('discord-reply')
 const client = new Discord.Client();
 const fs = require('fs');
 const { Collection } = require('discord.js');
 const prefix = require('./config/bot.json');
 
-fs.readdir('./events/', (err, files) => {
-  if (err) return console.log(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split('.')[0];
-    console.log(`Loading ${eventName}.js!`);
-    client.on(eventName, event.bind(null, client));
-  });
-});
-fs.readdir(`./commands/`, (error, files) => {
-  if (error) { return console.log("error i can not find commands"); };
-  files.forEach(file => {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+fs.readdir("./commands/", (err, categories) => {
+  if (err) console.log(err)
+  categories.forEach(category => {
+    let categoryName = category.split('.')[0];
+    fs.readdir(`./commands/${category}`, (error, files) => {
+      if (error) { return console.log("error i can not find commands"); };
+      files.filter(file => file.endsWith(".js")).forEach(file => {
+        const command = require(`./commands/${category}/${file}`);
+
+        client.commands.set(command.help.name, command)
+      })
+    })
+
   })
-});
+})
+
+fs.readdir('./events/', (err, categories) => {
+  if (err) return console.log(err);
+  categories.forEach(category => {
+    let categoryName = category.split('.')[0];
+    fs.readdir(`./events/${category}`, (error, files) => {
+      if (error) { return console.log("error i can not find commands"); };
+      files.filter(file => file.endsWith(".js")).forEach(file => {
+        var event = require(`./events/${category}/${file}`);
+        client.on(categoryName, event.bind(null, client))
+      })
+    })
+  })
 client.config = require('./config/bot.json');
 client.commands = new Discord.Collection();
 client.login(process.env.token);
