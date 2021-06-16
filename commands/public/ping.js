4fc6a8a915c4f2fe2cc0ce62396084e320c3415f
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2021-06-16 18:53:00 +0100

    Update ping.js

--- commands/public/ping.js
+++ commands/public/ping.js
@@ -2,7 +2,7 @@ module.exports = {
         name: "ping",//Command
         cooldown: 5,//cooldown in seconds
         aliases: ["timetaken","latency"],//Shortcuts,aliases You can add whatever you want
-        
+        ownerOnly:false,
     run: async(client, message, args) => {
       message.channel.send("pong!").then(m => m.edit(`\`\`\`js
 Ping: ${client.ws.ping}
